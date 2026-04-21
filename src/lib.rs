5b6536449094a517aa91ce45aee9b25fc17869e4
shafqat1841 <shafqat1841@gmail.com>
2026-04-21 12:13:39 +0500

    chore: break code into modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,26 @@
 mod errors;
 mod file_mmap;
-
-use std::{str::from_utf8};
+mod log_searcher;
+mod search_result;
 
 use memmap2::Mmap;
 
-use crate::{errors::RunErr, file_mmap::FileMmap};
-
-#[derive(Debug)]
-struct SearchResult<'a> {
-    line: &'a str,       // Reference to the full line in the NASA log
-    ip_address: &'a str, // Reference to just the IP part of that line
-}
+use crate::{errors::RunErr, file_mmap::FileMmap, log_searcher::LogSearcher};
 
 pub fn run() -> Result<(), RunErr> {
-    let address_to_find = "uplherc.upl.com";
+    let query = "uplherc.upl.com";
+    let limit: usize = 1000;
 
     let file_buffer: FileMmap = FileMmap::new()?;
 
     let bytes: &Mmap = file_buffer.get_bytes();
 
-    let bytes_split = bytes.split(|b| *b == b'\n');
+    let log_searcher = LogSearcher::new(bytes);
 
-    let filter_map_fun = |line_bytes| {
-        let line_str = from_utf8(line_bytes).ok()?;
-
-        let start_idx = line_str.find(address_to_find)?;
-
-        let end_idx = start_idx + address_to_find.len();
-        let ip_address = line_str.get(start_idx..end_idx)?;
-
-        Some(SearchResult {
-            line: line_str,
-            ip_address,
-        })
-    };
-
-    let result: Vec<SearchResult> = bytes_split
-        // for development we are just checking 1000 lines only
-        .take(1000)
-        .filter_map(filter_map_fun)
-        .collect();
+    let result = log_searcher.search(query, limit)?;
 
     for search_result in result {
-        println!("ip_address : {:?} ", search_result.ip_address);
-        println!("line : {:?} ", search_result.line);
+        println!("{}", search_result)
     }
 
     Ok(())
---
+++ src/log_searcher.rs
@@ -0,0 +1,45 @@
+use std::str::from_utf8;
+
+use memmap2::Mmap;
+
+use crate::{errors::RunErr, search_result::SearchResult};
+
+pub struct LogSearcher<'a> {
+    bytes: &'a Mmap,
+}
+
+impl<'a> LogSearcher<'a> {
+    pub fn new(bytes: &'a Mmap) -> Self {
+        Self { bytes }
+    }
+
+    pub fn get_search_result(&self, line_bytes: &'a [u8], query: &str) -> Option<SearchResult<'a>> {
+        let line_str = from_utf8(line_bytes).ok()?;
+
+        let start_idx = line_str.find(query)?;
+
+        let end_idx = start_idx + query.len();
+        let ip_address = line_str.get(start_idx..end_idx)?;
+
+        Some(SearchResult {
+            line: line_str,
+            ip_address,
+        })
+    }
+
+    pub fn search(&self, query: &str, limit: usize) -> Result<Vec<SearchResult<'a>>, RunErr> {
+        let bytes_split = self.bytes.split(|b| *b == b'\n');
+
+        let filter_map_fun = |line_bytes| {
+            self.get_search_result(line_bytes, query)
+        };
+
+        let result: Vec<SearchResult> = bytes_split
+            // for development we are just checking 1000 lines only
+            .take(limit)
+            .filter_map(filter_map_fun)
+            .collect();
+
+        Ok(result)
+    }
+}
