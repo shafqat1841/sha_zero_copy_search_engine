d72a141e3d6dc2a204e2c9948c67d1b8ba3c456a
shafqat1841 <shafqat1841@gmail.com>
2026-04-22 09:11:07 +0500

    fix: separate getting user args into a separate module

--- src/lib.rs
+++ src/lib.rs
@@ -2,56 +2,22 @@ mod errors;
 mod file_mmap;
 mod log_searcher;
 mod search_result;
+mod user_args;
 
 use memmap2::Mmap;
 
-use crate::{errors::RunErr, file_mmap::FileMmap, log_searcher::LogSearcher};
+use crate::{errors::RunErr, file_mmap::FileMmap, log_searcher::LogSearcher, user_args::UserArgs};
 
 pub fn run() -> Result<(), RunErr> {
-    let mut args = std::env::args();
-    args.next();
-    let path_opt: Option<String> = args.next();
-    let query_opt: Option<String> = args.next();
-    let limit_opt: Option<String> = args.next();
+    let user_args = UserArgs::new()?;
 
-    let mut limit: usize = 1000;
-
-    if let Some(limit_val) = limit_opt {
-        if let Ok(value) = limit_val.parse() {
-            limit = value
-        }
-    }
-
-    let query_val = match query_opt {
-        None => {
-            return Err(RunErr::EmptyQueryErr(
-                "Error: Empty query argument".to_string(),
-            ));
-        }
-        Some(value) => value,
-    };
-
-    let query: &str = &query_val;
-
-    let path_val = match path_opt {
-        None => {
-            return Err(RunErr::EmptyPathErr(
-                "Error: Empty path argument".to_string(),
-            ));
-        }
-        Some(value) => value,
-    };
-
-    // let path: &str = "./log_files/access.log";
-    let path: &str = &path_val;
-
-    let file_buffer: FileMmap = FileMmap::new(path)?;
+    let file_buffer: FileMmap = FileMmap::new(&user_args.path)?;
 
     let bytes: &Mmap = file_buffer.get_bytes();
 
     let log_searcher: LogSearcher = LogSearcher::new(bytes);
 
-    let result = log_searcher.search(query, limit)?;
+    let result = log_searcher.search(&user_args.query, user_args.limit)?;
 
     for search_result in result {
         println!("{}", search_result)
---
+++ src/user_args.rs
@@ -0,0 +1,54 @@
+use crate::errors::RunErr;
+
+pub struct UserArgs {
+    pub path: String,
+    pub query: String,
+    pub limit: usize,
+}
+
+impl UserArgs {
+    pub fn new() -> Result<Self, RunErr> {
+        let mut args = std::env::args();
+        args.next();
+        let path_opt: Option<String> = args.next();
+        let query_opt: Option<String> = args.next();
+        let limit_opt: Option<String> = args.next();
+
+        let mut limit: usize = 1000;
+
+        if let Some(limit_val) = limit_opt {
+            if let Ok(value) = limit_val.parse() {
+                limit = value
+            }
+        }
+
+        let query_val = match query_opt {
+            None => {
+                return Err(RunErr::EmptyQueryErr(
+                    "Error: Empty query argument".to_string(),
+                ));
+            }
+            Some(value) => value,
+        };
+
+        let query = query_val;
+
+        let path_val = match path_opt {
+            None => {
+                return Err(RunErr::EmptyPathErr(
+                    "Error: Empty path argument".to_string(),
+                ));
+            }
+            Some(value) => value,
+        };
+
+        // let path: &str = "./log_files/access.log";
+        let path = path_val;
+
+        Ok(Self {
+            path,
+            query,
+            limit,
+        })
+    }
+}
